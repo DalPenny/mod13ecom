e6e6cfadcfdf71bbb17c9af1f212d4de41b74f39
DalPenny <105093756+DalPenny@users.noreply.github.com>
2022-10-04 22:29:01 -0500

    first commit

---
+++ Develop/models/index.js
@@ -0,0 +1,27 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: ''
+})
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tags, {through: 'ProductTag' });
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {through: 'ProductTag' });
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
---
+++ Develop/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//connect to database before starting the express.js server
+sequelize.sync().then(() => {
+  app.listen(PORT, () => console.log(`Now listening at port ${PORT}`));
+}); //Need to check if this is required or not
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
